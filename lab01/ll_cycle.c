01f5da6572586a7610d6d10e268d0f8d26acc82f
ccmoony <cyj2003@sjtu.edu.cn>
2024-01-16 19:59:37 +0800

    lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,13 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortoise=head;
+    node *hare=head;
+    while (hare!=NULL && hare->next!=NULL){
+        tortoise=tortoise->next;
+        hare=hare->next->next;
+        if(tortoise==hare)
+            return 1;
+    }
     return 0;
 }
